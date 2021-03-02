8261dc14af572c5ccfd4c37be50d8cf35221700b
SirdsH <svatopluk.houserek@educanet.cz>
2021-03-02 13:26:46 +0100

    commit

---
+++ src/com/company/Animal.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Animal {
+    public int id;
+    public int type;
+    public String name;
+    public int age;
+    public String gender;
+    public String isagresive;
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	try {
+        BufferedReader br = new BufferedReader(new FileReader("animals.csv"));
+        BufferedReader brtype = new BufferedReader(new FileReader("animals_types.csv"));
+        String content = "";
+        String nextLine = br.readLine();
+        String typeLine = br.readLine();
+
+        ArrayList<String> arr = new ArrayList<>();
+
+        
+    }catch (IOException e){
+        System.out.println("Idk anymore");
+    }
     }
 }
